b31fcea5319f46a1457f722891de1b5c5b7c0a57
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-29 17:31:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,11 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_s?
+        combined_strings << src[row_index][element_index] + " "
+      end
+      element_index += 1 
     end
+    row_index += 1 
   end
+  combined_strings
 end
